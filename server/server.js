83d4a292fd357b45b391c36666fdbb796a09b723
Suryavardhan28 <suryavardhan28@gmail.com>
2024-06-27 02:00:50 +0530

    Added Mongoose Configs

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,11 +12,13 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/mernstack', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+const dbURI = process.env.DB_URI;
 
-// Routes
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+
 app.get('/api', (req, res) => {
   res.json('Hello, MERN stack!');
 });
